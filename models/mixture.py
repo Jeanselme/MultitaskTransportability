d9c88a9b32514b274e0612ef4d2646dff8172f02
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2022-07-30 11:56:59 +0100

    Push on cuda

--- models/mixture.py
+++ models/mixture.py
@@ -53,7 +53,7 @@ class Mixture(BatchForward):
         """
             Forward through the different networks
         """
-        temp_res, long_res, miss_res =  0, 0, 0
+        temp_res = long_res = miss_res = torch.zeros(1, device = h.get_device() if h.is_cuda else 'cpu')
         alphas = self.alphas(h[:, :-1])
         for j, (temp, long, miss) in enumerate(zip(self.temporal, self.longitudinal, self.missing)):
             alphas_repeat = alphas[:, :, j].unsqueeze(2).repeat(1, 1, self.outputdim)
@@ -64,7 +64,7 @@ class Mixture(BatchForward):
         return temp_res, long_res, miss_res, alphas
 
     def loss(self, h, x, i, m, l, batch = None, reduction = 'mean'):
-        loss_temp = loss_long = loss_miss = torch.zeros(1)
+        loss_temp = loss_long = loss_miss = torch.zeros(1, device = x.get_device() if x.is_cuda else 'cpu')
         alphas = self.alphas(h[:, :-1])
         for j, (temp, long, miss) in enumerate(zip(self.temporal, self.longitudinal, self.missing)):
             # Elbo loss (alpha could be computed exactly)
